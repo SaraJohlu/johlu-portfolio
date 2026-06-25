27ce5e6be7d7190770308d89041a27a39891bafc
Sara <sara_lunden1@hotmail.com>
2026-06-25 17:03:21 +0200

    New logo created

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,9 +3,9 @@ export default function Logo() {
 
     return(
         <>
-            <div className='flex flex-col items-start justify-center leading-none bg-transparent p-2'>
-                <p className='logo-main-fontstyle text-center'>Johlu</p>
-                <p className='logo-creation-fontstyle text-center'>Creation</p>
+            <div className='flex flex-col items-start justify-start leading-none bg-transparent p-2'>
+                <p className='logo-main-fontstyle w-full leading-none'>Johlu</p>
+                <p className='logo-creation-fontstyle w-2/3 text-center'>Creation</p>
             </div>
         </>
     )
